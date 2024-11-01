4448df9df0e564799e337a67c2d518ca0e49fe3e
s1lenter <sega29600@gmail.com>
2024-11-01 22:44:57 +0500

    Реализовать запись, изменение и удаление данных в БД

--- ResHelper/DB_client/models.py
+++ ResHelper/DB_client/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-
+class User(models.Model):
+    First_Name = models.CharField(max_length=20)
+    Last_Name = models.CharField(max_length=20)
--- ResHelper/DB_client/views.py
+++ ResHelper/DB_client/views.py
@@ -1,3 +1,46 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponseNotFound
+from .models import User
 
-# Create your views here.
+
+# получение данных из бд
+def index(request):
+    people = User.objects.all()
+    return render(request, "index.html", {"people": people})
+
+
+# сохранение данных в бд
+def create(request):
+    if request.method == "POST":
+        person = User()
+        person.First_Name = request.POST.get("First_Name")
+        person.Last_Name = request.POST.get("Last_Name")
+        person.save()
+    return HttpResponseRedirect("/")
+
+
+#изменение данных в бд
+def edit(request, id):
+    try:
+        person = User.objects.get(id=id)
+
+        if request.method == "POST":
+            person.First_Name = request.POST.get("First_Name")
+            person.Second_Name = request.POST.get("Second_Name")
+            person.save()
+            return HttpResponseRedirect("/")
+        else:
+            return render(request, "edit.html", {"person": person})
+    except User.DoesNotExist:
+        return HttpResponseNotFound("<h2>Person not found</h2>")
+
+
+# удаление данных из бд
+def delete(request, id):
+    try:
+        person = User.objects.get(id=id)
+        person.delete()
+        return HttpResponseRedirect("/")
+    except User.DoesNotExist:
+        return HttpResponseNotFound("<h2>Person not found</h2>")
+
